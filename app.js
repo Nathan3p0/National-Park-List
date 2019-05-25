dead3bacfe50ba921c6833ea7b6f47180aaa062f
Nathan Szelag <nszelag@live.com>
2019-05-24 23:07:19 -0400

    added initial files and created form elements in HTML doc

---
+++ app.js
@@ -0,0 +1,5 @@
+https://developer.nps.gov/api/v1/parks?stateCode=mi&fields=addresses&api_key=bSPKpQe0FYymu7sEEEqMJqqKYVppyTHzev2rq5pI
+
+
+const api_key = 'bSPKpQe0FYymu7sEEEqMJqqKYVppyTHzev2rq5pI';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
